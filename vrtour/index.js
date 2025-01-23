9c6af45a25dab486e822f36675ec1227041ab96f
thonguyen-1402 <anhthon570@gmail.com>
2025-01-24 05:32:49 +0700

    Update index.js

--- vrtour/index.js
+++ vrtour/index.js
@@ -209,7 +209,7 @@ AFRAME.registerComponent('move-controller', {   //mouse-click controller
                     arrow_forward.setAttribute("position", "-0.5 1.2 1");
                     arrow_right.setAttribute("rotation", "");
                     arrow_back.setAttribute("rotation", "0 1 -1");
-                    arrow_forward.setAttribute("rotation", "0 0 0");
+                    arrow_forward.setAttribute("rotation", "75 0 0");
 
                     info_icon.setAttribute("visible",false);
                 }
@@ -247,7 +247,7 @@ AFRAME.registerComponent('move-controller', {   //mouse-click controller
               arrow_forward.setAttribute("position", "-7 0 0");
           
 
-              arrow_right.setAttribute("rotation", "0 0 0");
+              arrow_right.setAttribute("rotation", "-90 90 0");
               arrow_forward.setAttribute("rotation", "0 90 0");
 
               info_icon.setAttribute("visible",true);
@@ -484,7 +484,7 @@ AFRAME.registerComponent('vr-controller', {   //mouse-click controller
 
                 arrow_right.setAttribute("rotation", "0 0 0");
                 arrow_back.setAttribute("rotation", "0 1 -1");
-                arrow_forward.setAttribute("rotation", "0 0 0");
+                arrow_forward.setAttribute("rotation", "75 0 0");
                
 
                 info_icon.setAttribute("visible", "false");
